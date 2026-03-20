82c7c2984d982e5fdc02cd9b29a9ac444268e74e
julia cereda <julia.cereda@aluno.senai.br>
2026-03-20 15:00:32 -0300

    COMITY: aula finalizada

--- numeros.js
+++ numeros.js
@@ -7,13 +7,13 @@ const botaoSorteio = document.querySelector('.area__button');
 const elementoNumero = document.querySelector('.area__numero');
 const listaNumero = document.querySelector('.historico__lista');
 const botaoLimpar = document.querySelector('.sorteador__limpar');
+const quantidadesNumeosRecentes = 5;
 
-const mensagem = document.querySelector('.area__mensagem')
- mensagem = innerHT
+const mensagem = document.querySelector('.area__mensagem');
 
 
 // console.log(vMax, vMin);
-
+//para atualizar para o valor escolhido
 const atualizarSlider = () =>{
     const min= Number(vMin.value);
     const max= Number(vMax.value);
@@ -34,4 +34,59 @@ const numeroAleatorio = (min, max) => {
     let numero = Math.floor(Math.random() * (max - min + 1));
     numero += min;
     return numero;
+};
+
+//para atualizar o textinho verde
+const atualizarTexto = (elemento, valor) =>{
+    elemento.textContent = valor;
+};
+
+//criar uma lista dos numeros sorteados
+const criarHistorico = (numero) =>{
+    const li = document.createElement('li');
+    li.textContent = numero;
+
+    li.addEventListener('click', () =>{
+        navigator.clipboard.writeText(numero);
+    });
+
+    return li;
+};
+
+//
+const atualizarHistorico = (lista, item, limite) =>{
+    lista.prepend(item);
+
+    if(lista.children.length > limite){
+        lista.removeChild(lista.lastChild);
+    }
+};
+
+const limparHistorico = () =>{
+    if(confirm('Deseja limpar o hitórico de sorteios?'));
+    listaNumero.textContent = '';
+    elementoNumero.textContent = '0';
 }
+botaoSorteio.addEventListener('click', () => {
+
+    const min= Number(vMin.value);
+    const max=Number(vMax.value);
+    //se o valor minimo for maior que o maximo
+    if (min>max) {
+        mensagem.textContent = 'O valor minimo deve ser igual ao maximo '
+        return
+    }
+    mensagem.textContent = '';
+
+    const numeroSorteado = numeroAleatorio(min, max);
+
+    atualizarTexto(elementoNumero,numeroSorteado);
+
+    const item = criarHistorico(numeroSorteado);
+    atualizarHistorico(listaNumero, item, quantidadesNumeosRecentes);
+
+
+}); 
+
+botaoLimpar.addEventListener('click', limparHistorico);
+//
